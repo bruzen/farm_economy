0a8cbd2660604508ded69d2c675e8b9d3865dc23
Terry Stewart <tcstewar@uwaterloo.ca>
2014-11-29 21:36:12 +0530

    Added comments

--- farm_economy/model_marketing.py
+++ farm_economy/model_marketing.py
@@ -1,5 +1,6 @@
 import farm_game.model
 
+# control how many farms there are
 farm_game.model.Model.farm_width = 3
 farm_game.model.Model.farm_height = 3
 farm_game.model.Model.farm_count = (farm_game.model.Model.farm_width *
@@ -8,7 +9,13 @@ farm_game.model.Model.farm_count = (farm_game.model.Model.farm_width *
 from gbm.models.core import Line
 
 
+# make a function that will run the model and return graphs
+#  the seed parameter sets the seed for the model
+#  the other parameters will eventually be sliders
+#  the output is plots to show
 def model_marketing(seed, certification_percent, organic_price):
+
+    # turn the sliders into interventions of the same form as in farm_game/actions.py
     interventions = [
         'subsidy:certification,%f' % certification_percent,
         'sd:peachesOrganicRedhaven,%f,0,0' % organic_price,
@@ -17,10 +24,12 @@ def model_marketing(seed, certification_percent, organic_price):
 
     code = 'init;' + ';'.join(interventions)
 
-    steps = 10
+    steps = 10   # number of steps to run the simulation for
 
+    # run the simulation
     data = farm_game.model.run(seed, code, *(['none'] * steps))
 
+    # extract plot information from all the data returned from the model
     plot1 = [
         Line(x=range(steps+2), y=data['prod_peachesRedhaven'], color='red', label='Redhaven'),
         Line(x=range(steps+2), y=data['prod_peachesOrganicRedhaven'], color='pink', label='Redhaven Organic'),
@@ -29,7 +38,7 @@ def model_marketing(seed, certification_percent, organic_price):
     return [plot1]
 
 
-
+# just for testing purposes, plot the results
 def show_plot(plots):
     import pylab
     for i, plot in enumerate(plots):
@@ -42,8 +51,8 @@ def show_plot(plots):
 
 
 if __name__ == '__main__':
+    # run the simulation with a particular slider value and seed
     plots = model_marketing(seed=1, certification_percent=0, organic_price=10)
-
     show_plot(plots)
 
 
